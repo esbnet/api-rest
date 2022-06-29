b67f5656c1ffccd601cee3a0fe130b217422041d
Edmilson Soares <esbnet@gmail.com>
2022-06-28 21:46:21 -0300

    feat: project complete

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.DB_CONNECT),
+    UsersModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
